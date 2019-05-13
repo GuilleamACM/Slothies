fbc3f8ad17bcf866fae2c6abfc4c99d4a4b46038
Lucas Miranda Lin <lucaslin@10-48-40-11.0101.ufpe.br.iosda.org>
2019-05-13 14:49:08 -0300

    Initial Commit

---
+++ Slothies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slothies
+//
+//  Created by Lucas Miranda Lin on 13/05/19.
+//  Copyright © 2019 Slothies Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
