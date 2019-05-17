30aa5bde0d002a7017bea5d4a70825c71676742d
cubsphere <cub@sphere.com>
2019-05-17 07:55:45 -0700

    improvements and finishing longUpdate logic for Slothometer, extracted a few constants to SharedConstants.swift

---
+++ Slothies/SharedConstants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Slothies
+//
+//  Created by cub on 16/05/19.
+//  Copyright © 2019 Slothies Inc. All rights reserved.
+//
+
+import Foundation
+
+let secondsPerDay = 86400
+let daysPerWeek = 7
--- Slothies/Slothometer.swift
+++ Slothies/Slothometer.swift
@@ -8,18 +8,16 @@
 
 import UIKit
 
-let secondsPerDay = 86400
-let daysPerWeek = 7
-
 class Slothometer {
     //deve incluir informacoes e metodos referentes ao funcionamento de um preguicometro
     
     //estes comentarios foram escritos sem muito pensamento
     //se achar melhor, sinta-se disposto a ignora-los
     
-    //max, min value of slothometer bar. temporary values
+    //key values for sloth
     static let maxValue: Int = secondsPerDay * daysPerWeek
     static let minValue: Int = 0
+    static let initValue: Int = secondsPerDay * (daysPerWeek - 1)
     
     //current total slothometer
     var totalValue: Int
@@ -28,20 +26,41 @@ class Slothometer {
     var individualValues: Dictionary<Sloth, Int>
     
     init () {
-        totalValue = secondsPerDay * (daysPerWeek - 1)
+        totalValue = Slothometer.initValue
         individualValues = Dictionary<Sloth, Int>()
+    }
+    
+    //currently, min of all sloth values
+    func updateTotalValue () {
+        var accu = Slothometer.maxValue
+        individualValues.keys.forEach {
+            accu = min(accu, individualValues[$0]!)
+        }
+        if accu <= Slothometer.minValue {
+            totalValue = Slothometer.minValue
+            //gameOver()
+        } else {
+            totalValue = accu
+        }
     }
     
     //place slothy into dictionary with initial value
     func addSloth (slothy: Sloth) {
-        individualValues[slothy] = secondsPerDay * (daysPerWeek - 1)
+        individualValues[slothy] = Slothometer.initValue
+        updateTotalValue()
+    }
+    
+    private func addSpecificValue(slothy: Sloth, val: Int) {
+        var result = individualValues[slothy]! + val
+        result = min(result, Slothometer.maxValue)
+        updateTotalValue()
     }
     
     //subtract seconds passed from values
     func longUpdate (prevTime: Date, currTime: Date) {
         let elapsed = Int(currTime.timeIntervalSince(prevTime))
         individualValues.keys.forEach {
-            individualValues[$0] = individualValues[$0]! - elapsed
+            addSpecificValue(slothy: $0, val: -elapsed)
         }
         updateTotalValue()
     }
@@ -49,16 +68,8 @@ class Slothometer {
     //check workout information, sum appropriate number into values
     func longUpdateSpecificValue (slothy: Sloth, info: Any) {
         //TODO: check workout information to see if everything is okay
-        individualValues[slothy] = individualValues[slothy]! + 2 * secondsPerDay
+        addSpecificValue(slothy: slothy, val: 2 * secondsPerDay)
+        //TODO: better define sloth reward for working out
         updateTotalValue()
-    }
-    
-    //currently, average all sloth values
-    func updateTotalValue () {
-        var accu = 0
-        individualValues.keys.forEach {
-            accu += individualValues[$0]!
-        }
-        totalValue = accu / individualValues.keys.count
     }
 }
