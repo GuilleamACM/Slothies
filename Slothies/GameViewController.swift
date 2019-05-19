19496a5ab9af9c62eaafc628700f87e22021d1ea
cubsphere <cub@sphere.com>
2019-05-19 15:27:01 -0700

    gamescreen interface basics

--- Slothies/GameViewController.swift
+++ Slothies/GameViewController.swift
@@ -9,15 +9,47 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    var room: RoomGroup?
+    var slothometer: Slothometer?
+    var food = 0
+    var coins = 0
 
+    @IBOutlet weak var SlothometerUIProgressView: UIProgressView!
+    
+    @IBOutlet weak var FoodLabel: UILabel!
+    
+    @IBOutlet weak var SlothyCoinsLabel: UILabel!
+    
+    @IBOutlet var SlothyButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        food = 100
+        coins = 100
+        updateInterface()
     }
     
-    func receiveData() {
-        
+    func updateInterface() {
+        FoodLabel.text = "\(food)"
+        SlothyCoinsLabel.text = "\(coins)"
+        SlothometerUIProgressView.setProgress(Float(slothometer!.totalValue / Slothometer.maxValue), animated: true)
+        loadRoom()
+    }
+    
+    func loadRoom() {
+        for (index, player) in room!.players.enumerated() {
+            if let _ = player {
+                let image = UIImage(named: "Slothy\(index+1) Face")
+                SlothyButtons[index].setBackgroundImage(nil, for: .normal)
+                SlothyButtons[index].setImage(image, for: .normal)
+            }
+        }
+    }
+    
+    func receiveData(room: RoomGroup) {
+        self.room = room
+        self.slothometer = room.slothGroup.slothometer
     }
 
     /*
