1c9cf8b02405f002ec3e36e239b6b7a13debae92
Henrique Andrade Mariz <riqmariz@10-48-48-116.0200.ufpe.br.iosda.org>
2019-05-20 10:27:42 -0300

    update in healthkit, now working correctly, needs to integrate with the rest of the project

--- Slothies/HealthHandler.swift
+++ Slothies/HealthHandler.swift
@@ -11,63 +11,42 @@ import HealthKit
 
 class HealthHandler{
     
-
-    private func authorizeHealthKit() {
-        
-        HealthKitSetupAssistant.authorizeHealthKit { (authorized, error) in
-            
-            guard authorized else {
-                
-                let baseMessage = "HealthKit Authorization Failed"
-                
-                if let error = error {
-                    print("\(baseMessage). Reason: \(error.localizedDescription)")
-                } else {
-                    print(baseMessage)
-                }
-                
-                return
-            }
-            
-            print("HealthKit Successfully Authorized.")
-        }
-        
-    }
+    static let singleton = HealthHandler()
     
-    func getWalkingRunningDistance(startDate: Date, endDate: Date, completion: @escaping (Double, Error?) -> () ){
+    func getWalkingRunningDistance(startDate: Date, endDate: Date, completion: @escaping (Double?, Error?) -> () ){
         
         let healthStore = HKHealthStore()
         
-        do{
-            guard let type = HKSampleType.quantityType(forIdentifier: .distanceWalkingRunning) else {
-                fatalError("Something went wrong retriebing quantity type distanceWalkingRunning")
-            }
+        
+        guard let type = HKSampleType.quantityType(forIdentifier: .distanceWalkingRunning) else {
+            fatalError("Something went wrong retriebing quantity type distanceWalkingRunning")
+        }
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
+        
+        let query = HKStatisticsQuery(quantityType: type, quantitySamplePredicate: predicate, options: [.cumulativeSum]) { (query, statistics, error) in
             
-            let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
-            
-            let query = HKStatisticsQuery(quantityType: type, quantitySamplePredicate: predicate, options: [.cumulativeSum]) { (query, statistics, error) in
-                var value: Double = 0
-                
-                if error != nil {
-                    print("something went wrong")
-                } else if let quantity = statistics?.sumQuantity() {
-                    value = quantity.doubleValue(for: HKUnit.meter())
-                }
+            if error != nil {
+                completion(nil,error)
+            } else if let quantity = statistics?.sumQuantity() {
+                let value = quantity.doubleValue(for: HKUnit.meter())
                 DispatchQueue.main.async {
                     completion(value, error)
                 }
             }
-            healthStore.execute(query)
+            
         }
+        healthStore.execute(query)
     }
     
-    func getStepsCount(startDate: Date, endDate: Date, completion: @escaping (Double, Error?) -> () ){
+    
+    func getStepsCount(startDate: Date, endDate: Date, completion: @escaping (Double?, Error?) -> () ){
         
         let healthStore = HKHealthStore()
         
         do{
             guard let type = HKSampleType.quantityType(forIdentifier: .stepCount) else {
-                fatalError("Something went wrong retriebing quantity type distanceWalkingRunning")
+                fatalError("Something went wrong retriebing quantity type stepsCount")
             }
             
             let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
@@ -87,6 +66,5 @@ class HealthHandler{
             healthStore.execute(query)
         }
     }
-    
     
 }
