b586cb49b487be74a11a39976471b9c904a6dcc0
cubsphere <6753972+cubsphere@users.noreply.github.com>
2019-05-27 17:07:17 -0300

    preparation for document conversion

---
+++ Slothies/Data Model/DocumentConversion.swift
@@ -0,0 +1,35 @@
+//
+//  DocumentConversion.swift
+//  Slothies
+//
+//  Created by Lucas Miranda Lin on 27/05/19.
+//  Copyright © 2019 Slothies Inc. All rights reserved.
+//
+
+import Foundation
+
+extension RoomGroup {
+    var dictionary {
+        return
+    }
+}
+
+/*
+ roomgroup.setData([
+ room name : String
+ room pass : String
+ room prevtime
+ slothgroup food
+ slothgroup distance
+ //if user = "" -> user = nil
+ //if name = "" -> sloth = nil
+ player0:  [user, lastUpdate, coins]
+ player1:  [user, lastUpdate, coins]
+ player2:  [user, lastUpdate, coins]
+ player3:  [user, lastUpdate, coins]
+ sloth0 : [name, sex, hunger, sleep, lastupdate, lastfed, lastslept, sloth]
+ sloth1: [name, sex, hunger, sleep, lastupdate, lastfed, lastslept, sloth]
+ sloth2: [name, sex, hunger, sleep, lastupdate, lastfed, lastslept, sloth]
+ sloth3: [name, sex, hunger, sleep, lastupdate, lastfed, lastslept, sloth]
+ ])
+ */
