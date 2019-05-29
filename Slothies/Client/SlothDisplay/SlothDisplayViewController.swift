6e065c3db6e8b91eef9e92a0d465721adf6a1096
cubsphere <6753972+cubsphere@users.noreply.github.com>
2019-05-29 17:01:33 -0300

    sad slothy when hungry

--- Slothies/Client/SlothDisplay/SlothDisplayViewController.swift
+++ Slothies/Client/SlothDisplay/SlothDisplayViewController.swift
@@ -341,10 +341,20 @@ class SlothDisplayViewController: UIViewController, GameDataUpdateable {
     func slothyIdleInterfaceInner() {
         var slothSprite: UIImage
         
-        if(slothy!.sex == .male) {
-            slothSprite = UIImage(named: "Male Slothy Idle")!
+        if(slothy!.hunger < Sloth.hungerMaxValue * 0.3 ||
+           slothy!.sleep < Sloth.sleepMaxValue * 0.3 ||
+            slothy!.sloth < Slothometer.maxValue) {
+            if(slothy!.sex == .male) {
+                slothSprite = UIImage(named: "Male Slothy Dying")!
+            } else {
+                slothSprite = UIImage(named: "Female Slothy Dying")!
+            }
         } else {
-            slothSprite = UIImage(named: "Female Slothy Idle")!
+            if(slothy!.sex == .male) {
+                slothSprite = UIImage(named: "Male Slothy Idle")!
+            } else {
+                slothSprite = UIImage(named: "Female Slothy Idle")!
+            }
         }
         SlothySprite.image = slothSprite
         SlothyButton.setImage(slothSprite, for: .normal)
