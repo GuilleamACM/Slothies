86d8e0e5d18bc016956d70b5050f03e73686ef34
vbas <vbas@cin.ufpe.br>
2019-05-29 22:45:07 -0300

    auto layout splash screen and light pink navbars

--- Slothies/Client/Main/MainViewController.swift
+++ Slothies/Client/Main/MainViewController.swift
@@ -37,6 +37,12 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         authorizeHealthKit()// authorization for the halthKit when the app is openned
+    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+        //CE4D72
+        self.navigationController?.navigationBar.tintColor = UIColor(red:0.81, green:0.30, blue:0.45, alpha:1.0)
 
     }
 
